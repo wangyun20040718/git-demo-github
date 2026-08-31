6dcd8a438af390eac01eacbdfe65e8550ed7ab84
王允 <wangyun@demo.com>
2026-08-31 21:45:35 +0800

    新增变量

--- test.c
+++ test.c
@@ -4,7 +4,11 @@ int main()
 {
     int nsb = 10;
     int nsa = 200;
+    int nsc = 0;
+    
+    nsc = nsa + nsb;
     printf("%d", nsa);
     printf("%d", nsb);
+    printf("%d", nsc);
     return 0;
 }

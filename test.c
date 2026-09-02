160109e3d5688e714e4a98978df813da60b2c698
wangyun <17516554+wangyun20040718@user.noreply.gitee.com>
2026-09-02 12:38:35 +0000

    update test.c.
    Signed-off-by: wangyun <17516554+wangyun20040718@user.noreply.gitee.com>

--- test.c
+++ test.c
@@ -5,10 +5,13 @@ int main()
     int nsb = 10;
     int nsa = 200;
     int nsc = 0;
-    
+    int *p;
+
+    *p = &nsa;
     nsc = nsa + nsb;
+
     printf("%d", nsa);
     printf("%d", nsb);
     printf("%d", nsc);
-
+    printf("%d",*p);
 }
